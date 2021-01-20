3f709b60293fb661ef33e99148b763f5831f1abd
Łukasz Dragon <lukasz.b.dragon@gmail.com>
2021-01-20 13:01:30 +0100

    Reading CLI metadata from Cargo.toml

--- huff/src/main.rs
+++ huff/src/main.rs
@@ -13,7 +13,10 @@ fn main() -> Result<(), error::Error>{
     let yaml = clap::load_yaml!("../res/cli.yml");
     let app = clap::App::from_yaml(
         yaml
-    );
+    )
+    .name(clap::crate_name!())
+    .version(clap::crate_version!())
+    .author(clap::crate_authors!());
 
     match cli::process_args(app.get_matches()){
         Ok(_) => Ok(()),
